59b98f78abe7166417792397a2650dcf997a43ad
Andrea Mazzullo <andrea.mazzullo@spindox.it>
2023-07-24 09:02:54 +0200

    add assets folders

---
+++ src/assets/galleries/index.js
@@ -0,0 +1,13 @@
+import foto1 from './home/Gallery-1,-foto-A.png';
+import foto2 from './home/Gallery-1,-foto-B.png';
+import foto3 from './home/Gallery-1,-foto-C.png';
+import foto4 from './home/Gallery-1,-foto-D.png';
+import foto5 from './home/Gallery-1,-foto-E.png';
+
+const galleries = {
+  home: [foto1, foto2, foto3, foto4, foto5],
+  famiglia: [],
+  territorio: [],
+};
+
+export default galleries;
