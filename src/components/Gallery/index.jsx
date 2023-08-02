c3f71ec121e65671edcfaaad05215a9470b1a843
Andrea Mazzullo <andrea.mazzullo@spindox.it>
2023-08-02 17:16:07 +0200

    fix fullscreen gallery

--- src/components/Gallery/index.jsx
+++ src/components/Gallery/index.jsx
@@ -4,14 +4,16 @@ import ImageGallery from 'react-image-gallery';
 import galleries from '../../assets/galleries';
 import { routes, NO_MENU_ROUTE_KEY } from '../../utils';
 import '../../../node_modules/react-image-gallery/styles/css/image-gallery.css';
+import './style.css';
 
 const Gallery = () => {
   const { pathname } = useLocation();
   let galleriesName = pathname.substring(1, pathname.length);
   if (!galleries[galleriesName]) galleriesName = 'tenuta';
-  console.log(galleriesName, pathname);
+  // console.log(galleriesName, pathname);
   const data = galleries[galleriesName].map(el => ({ original: el }));
-  return <ImageGallery items={data} fullScreen={true} />;
+
+  return <ImageGallery items={data} fullscreen useBrowserFullscreen width={'100%'} />;
 };
 
 export default Gallery;
