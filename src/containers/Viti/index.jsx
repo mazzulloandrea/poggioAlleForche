333936310adccf8d4044745eb74a5e073391ca22
mazzulloandrea@gmail.com <mazzulloandrea@gmail.com>
2024-01-15 11:18:28 +0100

    re-use mediaqueries librery

--- src/containers/Viti/index.jsx
+++ src/containers/Viti/index.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-// import { useMediaQuery } from 'react-responsive';
+import { useMediaQuery } from 'react-responsive';
 import ReactGA from 'react-ga4';
 import { Layout } from '..';
 import { Wrapper } from '../commonStyled';
@@ -16,21 +16,21 @@ import { Articles } from '../../components';
 const Viti = ({ lang, setLang }) => {
   const [show, setShow] = useState(false);
 
-  // const isBigScreen = useMediaQuery({ query: `(min-width: ${bigScreen}px)` });
-  // const isMediumScreen = useMediaQuery({
-  //   query: `(min-width: ${mediumScreen}px) and (max-width: ${bigScreen}px)`,
-  // });
-  // const isSmallScreen = useMediaQuery({
-  //   query: `(min-width: ${tabletWidth}px) and (max-width: ${mediumScreen}px)`,
-  // });
-  // const isTablet = useMediaQuery({
-  //   query: `(min-width: ${mobileWidth}px) and (max-width: ${tabletWidth}px)`,
-  // });
-  // const isMobile = useMediaQuery({
-  //   query: `(max-width: ${mobileWidth}px)`,
-  // });
+  const isBigScreen = useMediaQuery({ query: `(min-width: ${bigScreen}px)` });
+  const isMediumScreen = useMediaQuery({
+    query: `(min-width: ${mediumScreen}px) and (max-width: ${bigScreen}px)`,
+  });
+  const isSmallScreen = useMediaQuery({
+    query: `(min-width: ${tabletWidth}px) and (max-width: ${mediumScreen}px)`,
+  });
+  const isTablet = useMediaQuery({
+    query: `(min-width: ${mobileWidth}px) and (max-width: ${tabletWidth}px)`,
+  });
+  const isMobile = useMediaQuery({
+    query: `(max-width: ${mobileWidth}px)`,
+  });
 
-  // const isPortrait = useMediaQuery({ query: '(orientation: portrait)' });
+  const isPortrait = useMediaQuery({ query: '(orientation: portrait)' });
 
   useEffect(() => {
     ReactGA.send({ hitType: 'pageview', page: window.location.pathname, title: 'Cantina' });
@@ -43,12 +43,11 @@ const Viti = ({ lang, setLang }) => {
   const [dimensions, setDimensions] = useState({
     height: window.innerHeight,
     width: window.innerWidth,
-    ...getScreenDimensions(),
-    // isBigScreen,
-    // isMediumScreen,
-    // isSmallScreen,
-    // isTablet,
-    // isMobile,
+    isBigScreen,
+    isMediumScreen,
+    isSmallScreen,
+    isTablet,
+    isMobile,
     isPortrait: isScreenInPortrait(),
   });
 
@@ -57,12 +56,11 @@ const Viti = ({ lang, setLang }) => {
       setDimensions({
         height: window.innerHeight,
         width: window.innerWidth,
-        ...getScreenDimensions(),
-        // isBigScreen,
-        // isMediumScreen,
-        // isSmallScreen,
-        // isTablet,
-        // isMobile,
+        isBigScreen,
+        isMediumScreen,
+        isSmallScreen,
+        isTablet,
+        isMobile,
         isPortrait: isScreenInPortrait(),
       });
     };
